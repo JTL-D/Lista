13664ca1950f29c7da2447fd36adf41526092b7f
JTL-D <jimtag1987@gmail.com>
2025-09-26 15:46:03 +0300

    Update loader.js

--- js/loader.js
+++ js/loader.js
@@ -1,21 +1,8 @@
 // js/loader.js
-import ExcelJS from 'exceljs';
-
-window.loadFile = async function(type) {
-  const input = document.getElementById(
-    type === 'master' ? 'masterDropFile' : 'dataDropFile'
-  );
-  if (!input.files || !input.files[0]) return;
-
-  const nameId = type === 'master'
-    ? 'masterDropFileName' : 'dataDropFileName';
-  document.getElementById(nameId).textContent = input.files[0].name;
-
-  const buffer = await input.files[0].arrayBuffer();
-  const workbook = new ExcelJS.Workbook();
-  await workbook.xlsx.load(buffer);
-
-  window.dispatchEvent(new CustomEvent('excel-loaded', {
-    detail: { type, workbook }
-  }));
-};
+export async function loadWorkbook(file) {
+  // console.log('loadWorkbook', file);
+  const buffer = await file.arrayBuffer();
+  const wb = new ExcelJS.Workbook();
+  await wb.xlsx.load(buffer);
+  return wb;
+}
