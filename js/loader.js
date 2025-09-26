2884a0fce892a9b18a8d94c85b32232e60d57142
JTL-D <jimtag1987@gmail.com>
2025-09-26 10:01:31 +0300

    Update loader.js

--- js/loader.js
+++ js/loader.js
@@ -1,13 +1,12 @@
-
 // js/loader.js
-export async function Loader(ctx) {
-  const wbMaster = new ExcelJS.Workbook();
-  await wbMaster.xlsx.load(await ctx.files.master.arrayBuffer());
-  ctx.context.workbookMaster = wbMaster;
 
-  const wbInfo = new ExcelJS.Workbook();
-  await wbInfo.xlsx.load(await ctx.files.info.arrayBuffer());
-  ctx.context.workbookInfo = wbInfo;
-
-  return ctx;
+/**
+ * Φορτώνει ένα Excel workbook από File.
+ * @param {File} file
+ * @returns {Promise<ExcelJS.Workbook>}
+ */
+export async function loadWorkbook(file) {
+  const wb = new ExcelJS.Workbook();
+  await wb.xlsx.load(await file.arrayBuffer());
+  return wb;
 }
