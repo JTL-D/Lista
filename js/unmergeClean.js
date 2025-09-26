3209ace403d45e94443b3c355f1d52e574da8b67
JTL-D <jimtag1987@gmail.com>
2025-09-26 09:20:15 +0300

    Update unmergeClean.js

--- js/unmergeClean.js
+++ js/unmergeClean.js
@@ -1,1 +1,15 @@
 
+// js/unmergeClean.js
+export function unmergeAndClean(ctx) {
+  ctx.context.workbookMaster.worksheets.forEach(ws => {
+    ws._merges.forEach(range => ws.unMergeCells(range));
+    ws.eachRow(row => {
+      row.eachCell(cell => {
+        if (typeof cell.value === 'string') {
+          cell.value = cell.value.trim().toUpperCase();
+        }
+      });
+    });
+  });
+  return ctx;
+}
