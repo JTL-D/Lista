38a4f1c5f4b56eb8b36f3f9d8b8900cb237f0b49
JTL-D <jimtag1987@gmail.com>
2025-09-26 09:17:59 +0300

    Update loader.js

--- js/loader.js
+++ js/loader.js
@@ -1,1 +1,13 @@
 
+// js/loader.js
+export async function Loader(ctx) {
+  const wbMaster = new ExcelJS.Workbook();
+  await wbMaster.xlsx.load(await ctx.files.master.arrayBuffer());
+  ctx.context.workbookMaster = wbMaster;
+
+  const wbInfo = new ExcelJS.Workbook();
+  await wbInfo.xlsx.load(await ctx.files.info.arrayBuffer());
+  ctx.context.workbookInfo = wbInfo;
+
+  return ctx;
+}
